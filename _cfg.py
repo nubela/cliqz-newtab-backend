ab8da239ee3de19fefed03b072b90c0b02b1bbf6
nubela <nubela@gmail.com>
2014-10-08 17:10:34 +0800

    updated cfg template

--- _cfg.py
+++ _cfg.py
@@ -1,1 +1,8 @@
-SQL_URI_WEBAPP = "postgresql://nubela:040312@localhost/cliqz"
+import os
+
+
+SQL_URI_WEBAPP = "postgresql://user:pass@localhost/cliqz"
+CWD = os.path.dirname(os.path.realpath(__file__))
+UNSPLASH_IMAGE_DIR = os.path.join(CWD, "resources/unsplash_images")
+EXPIRE_UNSPLASH_DAYS = 1
+MEDIA_PATH = os.path.join(CWD, "cliqz/media").idea
